589e56bd5c28172eac167f5969fc5586ee2b1bfa
lazaro6almeida <lazaro6almeida@gmail.com>
2022-04-11 10:20:13 -0300

    First

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lazaro</title>
+        <script src="welcome.js"></script>
+</head>
+<body>
+
+<?php
+echo "My first PHP script and TS Script!";
+?>
+
+</body>
+</html>
